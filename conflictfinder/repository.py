6c4ddaa04a7452d7ceb62b9b6ac1cd562cbdb867
Geoff Mishkin <geoff@amsa.info>
2019-04-29 00:46:34 -0400

    Attempt at auto merge

--- conflictfinder/repository.py
+++ conflictfinder/repository.py
@@ -1,6 +1,8 @@
 import datetime
 import os
 import re
+import shutil
+import tempfile
 
 import git
 import yaml
@@ -28,6 +30,12 @@ class Repository(object):
             for exclude_rule in rc['exclude_branches']:
                 self._excluded_branches.append(re.compile(exclude_rule))
 
+    def get_base_branch(self):
+        return self._base_branch
+
+    def get_current_branch(self):
+        return self._gitrepo.head.ref
+
     def filter_branches(self):
         branches_ok_by_name = []
         excluded_branches = []
@@ -54,6 +62,48 @@ class Repository(object):
             branches_ok_by_base = branches_ok_by_name
 
         return branches_ok_by_base
+
+    def test_merge_current_branch(self):
+        index = git.index.base.IndexFile.from_tree(self._gitrepo,
+                self._gitrepo.merge_base(self.get_base_branch(), self.get_current_branch())[0],
+                self.get_base_branch(), self.get_current_branch()
+        )
+        unmerged_paths = self._auto_merge(index.unmerged_blobs())
+        if len(unmerged_paths) > 0:
+            raise Exception('Your branch has merge conflicts')
+
+        with tempfile.NamedTemporaryFile() as temp_index:
+            index.write(file_path=temp_index.name)
+            return index.commit('Conflict finder initial test merge', parent_commits=[self.get_base_branch().commit], head=False, skip_hooks=True)
+
+    def _auto_merge(self, blobs):
+        unmerged_paths = []
+        for path in blobs:
+            temp_files = {}
+            for (stage, blob) in path:
+                temp_files[stage] = tempfile.NamedTemporaryFile()
+
+            if len(temp_files) != 3: # Don't know what to do if not all three stages present
+                for stage in temp_files:
+                    temp_files[stage].close()
+                unmerged_paths.append(path)
+                continue
+
+            with temp_files[1], temp_files[2], temp_files[2]:
+                for stage in temp_files:
+                    shutil.copyfileobj(blob.data_stream, temp_files[stage])
+
+                try:
+                    self._gitrepo.git.merge_file('--stdout',
+                        temp_files[2].name, temp_files[1].name, temp_files[3].name
+                    )
+                except git.GitCommandError as e:
+                    if e.status > 0 and e.status <= 127:
+                        unmerged_paths.append(path)
+                    elif e.status >= 128:
+                        raise e
+
+        return unmerged_paths
 
     def _exclude_branch_by_patterns(self, branch):
         """Exclude branches that match a pattern excluded in the target repo config"""
